b306d8ffc54f4685ab517abeab93a810020df155
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-03 09:28:22 +0530

    Lab2

--- src/com/threads/Lab2.java
+++ src/com/threads/Lab2.java
@@ -1,4 +1,16 @@
 package com.threads;
 
 public class Lab2 {
+    public static void main(String []args){
+        Thread th = Thread.currentThread();
+        System.out.println(th);
+        for(int i=1;i<=5;i++){
+            System.out.println("i="+i+" by "+th.getName());
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException e){
+               System.out.println(e);
+            }
+        }
+    }
 }
